0fc029fe3b2a887983c45de6db91dc9116a473b0
Hayden Maudsley-Barton <hmaudsley-barton@scottlogic.com>
2025-09-05 11:57:39 +0100

    Add Tests For A

--- src/a/component.spec.ts
+++ src/a/component.spec.ts
@@ -10,4 +10,16 @@ describe('Calculator', () => {
     it('should exist', () => {
         expect(component).toBeTruthy();
     });
+
+    it('should add two numbers correctly', () => {
+        expect(component.add(2, 3)).toBe(5);
+    });
+
+    it('should subtract two numbers correctly', () => {
+        expect(component.subtract(5, 3)).toBe(2);
+    });
+
+    it('should sum an array of numbers correctly', () => {
+        expect(component.sum([1, 2, 3, 4])).toBe(10);
+    });
 });
