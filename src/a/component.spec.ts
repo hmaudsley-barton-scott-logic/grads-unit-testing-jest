ac67c421eac05c22e28d2be1fba6976cb9327edc
Hayden Maudsley-Barton <hmaudsley-barton@scottlogic.com>
2025-09-05 13:13:30 +0100

    toBe->toEqual

--- src/a/component.spec.ts
+++ src/a/component.spec.ts
@@ -8,14 +8,14 @@ describe('Calculator', () => {
     });
 
     it('should add two numbers correctly', () => {
-        expect(component.add(2, 3)).toBe(5);
+        expect(component.add(2, 3)).toEqual(5);
     });
 
     it('should subtract two numbers correctly', () => {
-        expect(component.subtract(5, 3)).toBe(2);
+        expect(component.subtract(5, 3)).toEqual(2);
     });
 
     it('should sum an array of numbers correctly', () => {
-        expect(component.sum([1, 2, 3, 4])).toBe(10);
+        expect(component.sum([1, 2, 3, 4])).toEqual(10);
     });
 });
