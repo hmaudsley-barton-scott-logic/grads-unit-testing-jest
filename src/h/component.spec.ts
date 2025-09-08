308b4edae0b534b65af18d3988ad342d32b050a7
Hayden Maudsley-Barton <hmaudsley-barton@scottlogic.com>
2025-09-08 16:58:54 +0100

    Tests For H

--- src/h/component.spec.ts
+++ src/h/component.spec.ts
@@ -5,12 +5,31 @@ describe('UserPortal', () => {
     let accountService: Partial<AccountService>;
 
     beforeEach(() => {
-        accountService = {};
+        const exampleUser = { id: 'foo', name: 'bar' };
+        accountService = {
+            get user() {
+                return exampleUser;
+            },
+            doLogin: jest.fn(),
+            doLogout: jest.fn()
+        };
         component = new UserPortal(<AccountService>accountService);
     });
 
-    it('should exist', () => {
-        expect(component).toBeTruthy();
+    it('should get user from our service', () => {
+        const exampleUser = { id: 'foo', name: 'bar' };
+        jest.spyOn(accountService, 'user', 'get').mockReturnValueOnce(exampleUser);
+        expect(component.user).toEqual(exampleUser);
+    });
+
+    it('should call the account service on login', () => {
+        component.login();
+        expect(accountService.doLogin).toHaveBeenCalledWith('foo');
+    });
+
+    it('should call the account service on logout', () => {
+        component.logout();
+        expect(accountService.doLogout).toHaveBeenCalled();
     });
 });
 
@@ -21,7 +40,23 @@ describe('AccountService', () => {
         service = new AccountService();
     });
 
-    it('should exist', () => {
-        expect(service).toBeTruthy();
+    it('should start with no user', () => {
+        expect(service.user).toBeUndefined();
+    });
+
+    it('should set a user on login', () => {
+        service.doLogin('foo');
+        expect(service.user).toEqual(expect.objectContaining({ id: 'foo' }));
+    });
+
+    it('should remove the user on logout', () => {
+        service.doLogin('foo');
+        expect(service.user).toBeDefined();
+        service.doLogout();
+        expect(service.user).toBeUndefined();
+    });
+
+    it('should not throw if logging out when not logged in', () => {
+        expect(() => service.doLogout()).not.toThrow();
     });
 });
