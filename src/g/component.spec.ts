6f8ca1f2579026da64987abf85f585f69b289c06
Hayden Maudsley-Barton <hmaudsley-barton@scottlogic.com>
2025-09-08 11:13:31 +0100

    Test Toggle For False

--- src/g/component.spec.ts
+++ src/g/component.spec.ts
@@ -3,19 +3,19 @@ import { ColumnHeaderParams, CustomColumnHeader } from './component';
 describe('CustomColumnHeader', () => {
     let component: CustomColumnHeader;
 
-    const mockParams = (): ColumnHeaderParams =>
+    const mockParams = (exp: boolean): ColumnHeaderParams =>
         <ColumnHeaderParams>{
             headerName: 'foo',
             api: {
                 isExpandable: () => true,
-                isExpanded: () => true,
+                isExpanded: () => exp,
                 setExpanded: (expanded: boolean) => expanded
             }
         };
 
     it('should get header name', () => {
         component = new CustomColumnHeader();
-        component.init(mockParams());
+        component.init(mockParams(true));
         expect(component.displayName).toEqual('foo');
     });
 
@@ -29,9 +29,16 @@ describe('CustomColumnHeader', () => {
         expect(component.isExpanded).toEqual(false);
     });
 
-    it('should call toggle()', () => {
+    it('should call toggle() for true', () => {
         component = new CustomColumnHeader();
-        component.init(mockParams());
+        component.init(mockParams(true));
+        component.toggle();
+        expect(component.toggle()).toHaveBeenCalled;
+    });
+
+    it('should call toggle() for false', () => {
+        component = new CustomColumnHeader();
+        component.init(mockParams(false));
         component.toggle();
         expect(component.toggle()).toHaveBeenCalled;
     });
