bf86f83c232378a4cf684848d0e7869b09202c87
Hayden Maudsley-Barton <hmaudsley-barton@scottlogic.com>
2025-09-08 08:58:47 +0100

    Tests for G

--- src/g/component.spec.ts
+++ src/g/component.spec.ts
@@ -1,13 +1,28 @@
-import { CustomColumnHeader } from './component';
+import { ColumnHeaderParams, CustomColumnHeader } from './component';
 
 describe('CustomColumnHeader', () => {
     let component: CustomColumnHeader;
 
-    beforeEach(() => {
+    const mockParams = (): ColumnHeaderParams =>
+        <ColumnHeaderParams>{
+            headerName: 'foo',
+            api: {
+                isExpandable: () => true,
+                isExpanded: () => true,
+                setExpanded: (expanded: boolean) => expanded
+            }
+        };
+
+    it('should get header name', () => {
         component = new CustomColumnHeader();
+        component.init(mockParams());
+        expect(component.displayName).toEqual('foo');
     });
 
-    it('should exist', () => {
-        expect(component).toBeTruthy();
+    it('should call toggle()', () => {
+        component = new CustomColumnHeader();
+        component.init(mockParams());
+        component.toggle();
+        expect(component.toggle()).toHaveBeenCalled;
     });
 });
